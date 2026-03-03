80f72ea94f9c648375c7d4dd5e1f99540b3a367e
Badal sah <badal.shah@quantumarcadia.com>
2026-03-03 18:05:34 +0530

    added Build linux server to build script

--- Unity_Jenkins/Assets/Scripts/Editor/BuildScript.cs
+++ Unity_Jenkins/Assets/Scripts/Editor/BuildScript.cs
@@ -1,64 +1,239 @@
+// using System.IO;
+// using UnityEditor;
+// using System.Linq;
+// using System.IO.Compression;
+// using System;
+
+// public class BuildScript
+// {
+//     public static void BuildWindows()
+//     {
+//         string path = "Builds/Windows";
+//         CreateDirectory(path);
+
+//         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+//         {
+//             scenes = GetEnabledScenes(),
+//             locationPathName = $"{path}/MyGame.exe",
+//             target = BuildTarget.StandaloneWindows64,
+//             options = BuildOptions.None
+//         };
+
+//         BuildPipeline.BuildPlayer(buildPlayerOptions);
+//         ZipBuild(path);
+//     }
+    
+//     public static void BuildAndroid()
+// {
+//     string[] args = Environment.GetCommandLineArgs();
+//     string buildType = GetArgument(args, "-buildType");
+
+//     bool isAAB = buildType == "AAB";
+
+//     string folderPath = isAAB ? "Builds/AndroidAAB" : "Builds/AndroidAPK";
+//     string fileName = isAAB ? "MyGame.aab" : "MyGame.apk";
+
+//     CreateDirectory(folderPath);
+
+//     string fullPath = Path.Combine(folderPath, fileName);
+
+//     if (File.Exists(fullPath))
+//         File.Delete(fullPath);
+
+//     EditorUserBuildSettings.buildAppBundle = isAAB;
+
+//     BuildPlayerOptions options = new BuildPlayerOptions
+//     {
+//         scenes = GetEnabledScenes(),
+//         locationPathName = fullPath,
+//         target = BuildTarget.Android,
+//         options = BuildOptions.None
+//     };
+
+//     BuildPipeline.BuildPlayer(options);
+// }
+
+//     public static void CreateDirectory(string path)
+//     {
+//         if (!Directory.Exists(path))   
+//         {
+//             Directory.CreateDirectory(path);
+//         }
+//     }
+
+//     private static string[] GetEnabledScenes()
+//     {
+//         return EditorBuildSettings.scenes
+//             .Where(scene => scene.enabled)
+//             .Select(scene => scene.path)
+//             .ToArray();
+//     }
+
+//     private static void ZipBuild(string buildPath)
+//     {
+//         string zipPath = buildPath + ".zip";
+
+//         if (File.Exists(zipPath))
+//         {
+//             File.Delete(zipPath);
+//         }
+
+//         ZipFile.CreateFromDirectory(buildPath, zipPath);
+//     }
+
+//     private static string GetArgument(string[] args, string name)
+//     {
+//         for (int i = 0; i < args.Length; i++)
+//         {
+//             if (args[i] == name && i + 1 < args.Length)
+//             {
+//                 return args[i + 1];
+//             }
+//         }
+//         return null;
+//     }
+// }
+
+
 using System.IO;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 using System.Linq;
-using System.IO.Compression;
 using System;
+using System.IO.Compression;
+using UnityEngine;
 
 public class BuildScript
 {
+    // ===========================
+    // WINDOWS BUILD
+    // ===========================
     public static void BuildWindows()
     {
-        string path = "Builds/Windows";
-        CreateDirectory(path);
+        string folderPath = "Builds/Windows";
+        CreateDirectory(folderPath);
 
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        string exePath = Path.Combine(folderPath, "MyGame.exe");
+
+        if (File.Exists(exePath))
+            File.Delete(exePath);
+
+        BuildPlayerOptions options = new BuildPlayerOptions
         {
             scenes = GetEnabledScenes(),
-            locationPathName = $"{path}/MyGame.exe",
+            locationPathName = exePath,
             target = BuildTarget.StandaloneWindows64,
             options = BuildOptions.None
         };
 
-        BuildPipeline.BuildPlayer(buildPlayerOptions);
-        ZipBuild(path);
-    }
-    
-    public static void BuildAndroid()
-{
-    string[] args = Environment.GetCommandLineArgs();
-    string buildType = GetArgument(args, "-buildType");
+        BuildReport report = BuildPipeline.BuildPlayer(options);
 
-    bool isAAB = buildType == "AAB";
-
-    string folderPath = isAAB ? "Builds/AndroidAAB" : "Builds/AndroidAPK";
-    string fileName = isAAB ? "MyGame.aab" : "MyGame.apk";
-
-    CreateDirectory(folderPath);
-
-    string fullPath = Path.Combine(folderPath, fileName);
-
-    if (File.Exists(fullPath))
-        File.Delete(fullPath);
-
-    EditorUserBuildSettings.buildAppBundle = isAAB;
-
-    BuildPlayerOptions options = new BuildPlayerOptions
-    {
-        scenes = GetEnabledScenes(),
-        locationPathName = fullPath,
-        target = BuildTarget.Android,
-        options = BuildOptions.None
-    };
-
-    BuildPipeline.BuildPlayer(options);
-}
-
-    public static void CreateDirectory(string path)
-    {
-        if (!Directory.Exists(path))   
+        if (report.summary.result == BuildResult.Succeeded)
         {
-            Directory.CreateDirectory(path);
+            ZipBuild(folderPath);
+            EditorApplication.Exit(0);
         }
+        else
+        {
+            Debug.LogError("Windows Build Failed");
+            EditorApplication.Exit(1);
+        }
+    }
+
+    // ===========================
+    // ANDROID BUILD (APK / AAB)
+    // ===========================
+    public static void BuildAndroid()
+    {
+        string[] args = Environment.GetCommandLineArgs();
+        string buildType = GetArgument(args, "-buildType");
+
+        if (string.IsNullOrEmpty(buildType))
+        {
+            Debug.LogError("Missing -buildType argument (APK or AAB)");
+            EditorApplication.Exit(1);
+            return;
+        }
+
+        bool isAAB = buildType.ToUpper() == "AAB";
+
+        string folderPath = isAAB ? "Builds/AndroidAAB" : "Builds/AndroidAPK";
+        string fileName = isAAB ? "MyGame.aab" : "MyGame.apk";
+
+        CreateDirectory(folderPath);
+
+        string fullPath = Path.Combine(folderPath, fileName);
+
+        if (File.Exists(fullPath))
+            File.Delete(fullPath);
+
+        EditorUserBuildSettings.buildAppBundle = isAAB;
+
+        PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel25;
+        PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevel34;
+
+        BuildPlayerOptions options = new BuildPlayerOptions
+        {
+            scenes = GetEnabledScenes(),
+            locationPathName = fullPath,
+            target = BuildTarget.Android,
+            options = BuildOptions.None
+        };
+
+        BuildReport report = BuildPipeline.BuildPlayer(options);
+
+        if (report.summary.result == BuildResult.Succeeded)
+        {
+            EditorApplication.Exit(0);
+        }
+        else
+        {
+            Debug.LogError("Android Build Failed");
+            EditorApplication.Exit(1);
+        }
+    }
+
+    // ===========================
+    // LINUX HEADLESS SERVER
+    // ===========================
+    public static void BuildLinuxServer()
+    {
+        string folderPath = "Builds/LinuxServer";
+        CreateDirectory(folderPath);
+
+        string serverPath = Path.Combine(folderPath, "MyServer.x86_64");
+
+        if (File.Exists(serverPath))
+            File.Delete(serverPath);
+
+        BuildPlayerOptions options = new BuildPlayerOptions
+        {
+            scenes = GetEnabledScenes(),
+            locationPathName = serverPath,
+            target = BuildTarget.StandaloneLinux64,
+            options = BuildOptions.EnableHeadlessMode
+        };
+
+        BuildReport report = BuildPipeline.BuildPlayer(options);
+
+        if (report.summary.result == BuildResult.Succeeded)
+        {
+            EditorApplication.Exit(0);
+        }
+        else
+        {
+            Debug.LogError("Linux Server Build Failed");
+            EditorApplication.Exit(1);
+        }
+    }
+
+    // ===========================
+    // HELPER METHODS
+    // ===========================
+    private static void CreateDirectory(string path)
+    {
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
     }
 
     private static string[] GetEnabledScenes()
@@ -69,16 +244,14 @@ public class BuildScript
             .ToArray();
     }
 
-    private static void ZipBuild(string buildPath)
+    private static void ZipBuild(string folderPath)
     {
-        string zipPath = buildPath + ".zip";
+        string zipPath = folderPath + ".zip";
 
         if (File.Exists(zipPath))
-        {
             File.Delete(zipPath);
-        }
 
-        ZipFile.CreateFromDirectory(buildPath, zipPath);
+        ZipFile.CreateFromDirectory(folderPath, zipPath);
     }
 
     private static string GetArgument(string[] args, string name)
@@ -86,9 +259,7 @@ public class BuildScript
         for (int i = 0; i < args.Length; i++)
         {
             if (args[i] == name && i + 1 < args.Length)
-            {
                 return args[i + 1];
-            }
         }
         return null;
     }
